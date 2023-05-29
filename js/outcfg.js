7b027bedd8e2a7c84ed696fe6c4d373f5aba2ef9
zouzhe <234876317@qq.com>
2023-05-29 11:35:45 +0800

    feat: mock data

--- js/outcfg.js
+++ js/outcfg.js
@@ -1,15 +1,15 @@
 window.mapConfig={
-   "websocketUrl": 'ws://www.coolhtml5.com:3000/ws',//socket路径
-   "isDebug": !0,//是否是测试，默认不是
-   "topAjaxUrl":"http://www.coolhtml5.com:3000/top5",
-   "topIpAjaxUrl":"http://www.coolhtml5.com:3000/topIp5",
-   "topDomAjaxUrl":"http://www.coolhtml5.com:3000/topDom5",
-   "colors": {
-       0: {color: "#f80000", name: "SQL注入"},
-       1: {color: "#01befd", name: "XSS"},
-       2: {color: "#ffd724", name: "扫描攻击"},
-       3: {color: "#0050ff", name: "WEB漏洞"},
-       4: {color: "#cc9999", name: "自定义攻击"},
-       5: {color: "#01c602", name: "其他"}
-   }
-};
+    "websocketUrl": 'ws://www.coolhtml5.com:3000/ws',//socket路径
+    "isDebug": !0,//是否是测试，默认不是
+    "topAjaxUrl":"./mock/top.json",
+    "topIpAjaxUrl":"./mock/ip.json",
+    "topDomAjaxUrl":"./mock/domain.json",
+    "colors": {
+        0: {color: "#f80000", name: "SQL注入"},
+        1: {color: "#01befd", name: "XSS"},
+        2: {color: "#ffd724", name: "扫描攻击"},
+        3: {color: "#0050ff", name: "WEB漏洞"},
+        4: {color: "#cc9999", name: "自定义攻击"},
+        5: {color: "#01c602", name: "其他"}
+    }
+ };
